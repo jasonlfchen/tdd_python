ea71eb46b39fab48e0576bcf8d61d7394f46c64c
Jason Chen <jchen7960@gmail.com>
2015-11-19 13:58:15 -0800

    Update functional test to enter 2 items to list

--- functional_test.py
+++ functional_test.py
@@ -19,25 +19,31 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         
         #Then Edith enters a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-                inputbox.get_attribute('placeholder'),'Enter a to-do item')
-
         #And Edith types "Buy peacock feathers" into a text box
-        inputbox.send_keys('Buy peacock feathers')
-
         #When Edith hits enter, the page updates and lists a to-do table
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        
+        #There is an additional text box inviting Edith to add another item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows),
-                "New to-do item did not appear in table"
-        )
+        #Then the page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         #Edit needs to enter another item
         self.fail('Finish the test!')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
