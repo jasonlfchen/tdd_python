f26dc07cfebd31e9a37b2f8fb772408d4bd4ea03
Jason Chen <jchen7960@gmail.com>
2016-01-04 17:20:22 -0600

    Add bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -78,7 +78,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.set_window_size(1024, 768)
 
         #She notices the input box is nicely centered
-        inputbox.send_keys('testing\n')
+        self.browser.find_element_by_id('id_new_item').send_keys('testing\n')
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
